149e0a918dac647a433b4222f589d577b6eb2207
mahi <mahi@gmail.com>
2022-01-05 10:42:55 +0000

    Login page is completed

--- c2.java
+++ c2.java
@@ -1,3 +1,4 @@
 Hii This is mahesh. I am working as frontend developer!!
 I have committed the changes and login page is working fine !!
 New feature is updated!! Waiting for conformation!
+Thanks for the co operation! Code is working fine .
